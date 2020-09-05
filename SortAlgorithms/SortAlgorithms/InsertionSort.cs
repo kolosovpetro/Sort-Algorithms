e3e068845b255bbeb4c4c054d3ff2286d15f1e25
razumovskiy <kolosovp94@gmail.com>
2020-09-05 21:04:26 +0300

    insertion and selection sorts are implemented

---
+++ SortAlgorithms/SortAlgorithms/InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿using SortAlgorithms.Auxiliaries.Auxiliaries;
+using SortAlgorithms.Interfaces;
+
+namespace SortAlgorithms.SortAlgorithms
+{
+    public class InsertionSort : ISortAlgorithm
+    {
+        public int[] SortSequence(int[] sequence)
+        {
+            var copyArray = ArrayDeepCopy.CopyArray(sequence);
+
+            for (var k = 1; k < copyArray.Length; k++)
+            {
+                var j = k;
+
+                while (j > 1 && copyArray[j - 1] > copyArray[j])
+                {
+                    ISortAlgorithm.Swap(copyArray, j - 1, j);
+                    j--;
+                }
+            }
+
+            return copyArray;
+        }
+    }
+}
---
+++ SortAlgorithms/SortAlgorithms/SelectionSort.cs
@@ -0,0 +1,25 @@
+﻿using SortAlgorithms.Auxiliaries.Auxiliaries;
+using SortAlgorithms.Interfaces;
+
+namespace SortAlgorithms.SortAlgorithms
+{
+    public class SelectionSort : ISortAlgorithm
+    {
+        public int[] SortSequence(int[] sequence)
+        {
+            var copyArray = ArrayDeepCopy.CopyArray(sequence);
+
+            for (var k = 0; k < copyArray.Length; k++)
+            {
+                var minIndex = k;
+
+                for (var j = k + 1; j < copyArray.Length; j++)
+                    minIndex = copyArray[minIndex] < copyArray[j] ? minIndex : j;
+                
+                ISortAlgorithm.Swap(copyArray, k, minIndex);
+            }
+
+            return copyArray;
+        }
+    }
+}
