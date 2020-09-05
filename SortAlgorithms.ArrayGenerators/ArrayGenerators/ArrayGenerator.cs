761f86fdd25a2a78a30568e3ca337cae8e46a791
razumovskiy <kolosovp94@gmail.com>
2020-09-05 20:31:50 +0300

    array generators created

---
+++ SortAlgorithms.ArrayGenerators/ArrayGenerators/ArrayGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace SortAlgorithms.ArrayGenerators.ArrayGenerators
+{
+    public static class ArrayGenerator
+    {
+        private static readonly Random Random = new Random();
+
+        public static int[] RandomArray(int size)
+        {
+            var array = new int[size];
+
+            for (var i = 0; i < size; i++)
+                array[i] = Random.Next(size);
+
+            return array;
+        }
+
+        public static int[] AShapeArray(int size)
+        {
+            var left = RandomArray(size / 2);
+            Array.Sort(left);
+            var right = RandomArray(size - size / 2);
+            Array.Sort(right);
+            Array.Reverse(right);
+            return left.Concat(right).ToArray();
+        }
+
+        public static int[] VShapeArray(int size)
+        {
+            var left = RandomArray(size / 2);
+            Array.Sort(left);
+            Array.Reverse(left);
+            var right = RandomArray(size - size / 2);
+            Array.Sort(right);
+            return left.Concat(right).ToArray();
+        }
+
+        public static int[] ConstantArray(int size)
+        {
+            var value = Random.Next(size);
+            var array = new int[size];
+
+            for (var i = 0; i < size; i++)
+                array[i] = value;
+
+            return array;
+        }
+
+        public static int[] ReverseArray(int size)
+        {
+            var array = RandomArray(size);
+            Array.Sort(array);
+            Array.Reverse(array);
+            return array;
+        }
+
+        public static int[] SortedArray(int size)
+        {
+            var array = RandomArray(size);
+            Array.Sort(array);
+            return array;
+        }
+    }
+}
---
+++ SortAlgorithms.ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using SortAlgorithms.ArrayGenerators.ArrayGenerators;
+
+namespace SortAlgorithms.ConsoleUI
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var randomArray = ArrayGenerator.RandomArray(10);
+            var aShapeArray = ArrayGenerator.AShapeArray(10);
+
+            Console.WriteLine("Random array: ");
+            foreach (var value in randomArray)
+            {
+                Console.Write(value + " ");
+            }
+
+            Console.WriteLine("AShape Array: ");
+            foreach (var value in aShapeArray)
+            {
+                Console.Write(value + " ");
+            }
+        }
+    }
+}
